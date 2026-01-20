34c98f42f79a4e9fa72c8e45cb5fe543caa9b3f3
its-mohan79 <mohanroy8989@gmail.com>
2026-01-20 23:43:49 +0530

    done project 2

---
+++ The perfect guess.py
@@ -0,0 +1,15 @@
+import random
+n = random.randint(1,100)
+a = -1
+guesses = 1
+while(a != n):
+    a = int(input("Enter your guess between 1 and 100: "))
+    if (a>n):
+        print("Too low! Try again.")
+        guesses +=1
+    elif (a<n):
+        print("Too high! Try again.")
+        guesses +=1
+
+print(f"Congratulations! You've guessed the number {n} in {guesses} attempts")
+    
